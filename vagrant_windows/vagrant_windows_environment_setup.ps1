dc279ed717dcf755fd9ec631e8bb13cbb90c2171
Gary Butler <gary.butler@ring0.com>
2021-07-21 13:38:56 -0500

    Corrected file path concatenations.

--- vagrant_windows/vagrant_windows_environment_setup.ps1
+++ vagrant_windows/vagrant_windows_environment_setup.ps1
@@ -2,7 +2,7 @@
 
 function File-Check
 {
-    Push-Location $DOCUMENTS + '\sensor_transport_team_certs'
+    Push-Location $DOCUMENTS'\sensor_transport_team_certs'
     $ret_val = [bool](& 'C:\Program Files\Git\usr\bin\sha256sum.exe' -c sensor_transport_team_key_passphrases.sha256)
     Pop-Location
     return $ret_val
@@ -17,7 +17,7 @@ if(($cert_zip = (gci -File ..\Downloads\sensor_transport_team_certs.zip -ErrorAc
 start-service -name ssh-agent
 get-service -name ssh-agent
 
-Push-Location $DOCUMENTS + '\sensor_transport_team_certs'
+Push-Location $DOCUMENTS'\sensor_transport_team_certs'
 ssh-add .\sensor_transport_dev_team .\sensor_transport_team
 Pop-Location
 
@@ -27,7 +27,7 @@ Copy-Item Z:\known_hosts C:\Users\IEUser\.ssh\
 & 'C:\Program Files\Git\bin\git.exe' config --global core.sshCommand C:/Windows/System32/OpenSSH/ssh.exe
 & 'C:\Program Files\Git\bin\git.exe' clone git@github.com:mrgarybutler/sensor_transport_dev.git
 
-Push-Location $DOCUMENTS + '\sensor_transport_dev'
+Push-Location $DOCUMENTS'\sensor_transport_dev'
 #Get-Content README.md
 Write-Host $env:CERT_PASS
 Write-Host $env:ACCESS_KEY_ID
@@ -37,6 +37,7 @@ Write-Host $env:PRIVATE_KEY_PATH
 pause
 & 'C:\HashiCorp\Vagrant\bin\vagrant.exe' plugin install --plugin-version 1.0.1 fog-ovirt
 & 'C:\HashiCorp\Vagrant\bin\vagrant.exe' plugin install vagrant-aws
-Copy-Item -Force $DOCUMENTS + '\vagrant_launch_host\connect_aws.rb' C:\Users\IEUser\.vagrant.d\gems\2.6.6\gems\vagrant-aws-0.7.2\lib\vagrant-aws\action\connect_aws.rb
+$connect_aws = (gci -recurse -Path C:\Users\IEUser\.vagrant.d\gems connect_aws.rb).FullName
+Copy-Item -Force '..\vagrant_launch_host\vagrant_windows\connect_aws.rb' $connect_aws
 & 'C:\HashiCorp\Vagrant\bin\vagrant.exe' up st_zero
 # Restart-Computer -Confirm -Verbose
